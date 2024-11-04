de5e64458a92e1f8ce61333630fe903b770a48d7
Fabian Velazquez <fabiian.vela@gmail.com>
2024-11-04 09:53:31 -0600

    Commit del modelo Oficio

--- app/Models/Oficio.php
+++ app/Models/Oficio.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Oficio extends Model
 {
-    //
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'folio_oficio',
+        'elabora',
+        'estatus_oficio',
+        'fecha_oficio',
+        'fecha_recepcion',
+        'asunto',
+        'firma',
+        'dependencia',
+        'dirigido_a',
+        'cargo',
+        'tipo_oficio',
+        'oficio_procedencia',
+        'turnado_a',
+        'recibe',
+        'es_dirigido',
+        'con_copia',
+        'girado_delegacion',
+        'observaciones',
+        'estatus',
+    ];
 }
